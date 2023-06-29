0c469a7e7b80a4c3bec800a09d6410ffe5efc515
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-29 16:46:28 +0530

    Initial Commit

---
+++ ClinicDemo/ClinicDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicDemoApp.swift
+//  ClinicDemo
+//
+//  Created by Bhumika Patel on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ClinicDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
