069eecdcb51e4cc131ee312c3ea52f8bbe23dc6f
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-07-04 16:27:10 +0530

    manage error handling(network and session) and instance of apimanager

--- ClinicDemo/ClinicViewModel.swift
+++ ClinicDemo/ClinicViewModel.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Bhumika Patel on 29/06/23.
 //
-
+//
 import Foundation
 import Combine
 
@@ -14,53 +14,62 @@ class ClinicViewModel: ObservableObject {
     @Published var todayTimeSlots: [TimeSlot] = []
     @Published var tomorrowTimeSlots: [TimeSlot] = []
     @Published var showErrorDialog = false
-    
-    func loadClinics() {
-        guard let url = URL(string: "https://api.care-uat.psjhealth.org/v1/departments") else { return }
-        URLSession.shared.dataTask(with: url) { data, _, _ in
-            if let data = data {
-                do {
-                    let clinics = try JSONDecoder().decode([Clinic].self, from: data)
-                    DispatchQueue.main.async {
-                        self.clinics = clinics
-                    }
-                } catch {
-                    print(error)
-                }
-            }
-        }
-        .resume()
-    }
-    
-    func loadTimeSlots(for clinic: Clinic) {
-        guard let url = URL(string: "https://api.care-uat.psjhealth.org/v2/departments/\(clinic.urlName)/timeslots?visitTypeName=Illness") else { return }
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            if let data = data {
-                do {
-                    let response = try JSONDecoder().decode(TimeslotsResponse.self, from: data)
-//                    print(response)
-                    DispatchQueue.main.async {
-                        if let firstDay = response.scheduleDays.first {
-                            self.todayTimeSlots = firstDay.timeSlots
-                        } else {
-                            self.todayTimeSlots = []
-                        }
-                        if let secondDay = response.scheduleDays.dropFirst().first {
-                            self.tomorrowTimeSlots = secondDay.timeSlots
-                        } else {
-                            self.tomorrowTimeSlots = []
-                        }
-                        
-                        if self.todayTimeSlots.isEmpty && self.tomorrowTimeSlots.isEmpty {
-                            self.showErrorDialog = true
-                        }
-                    }
-                } catch {
-                    print(error)
-                }
-            }
-        }
-        .resume()
-    }
-}
+    @Published var errorMessage: String = ""
+    @Published var title: String = ""
+    private var cancellables: Set<AnyCancellable> = []
+    let apiManager: APIManager
 
+    init(apiManager: APIManager = APIManager()) {
+        self.apiManager = apiManager
+    }
+
+    func loadClinics() {
+        apiManager.fetchClinics()
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.handleError(error)
+                }
+            } receiveValue: { [weak self] clinics in
+                self?.clinics = clinics
+            }
+            .store(in: &cancellables)
+    }
+
+    func loadTimeSlots(for clinic: Clinic) {
+        apiManager.fetchTimeSlots(for: clinic)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.handleError(error)
+                }
+            } receiveValue: { [weak self] response in
+                if let firstDay = response.scheduleDays.first {
+                    self?.todayTimeSlots = firstDay.timeSlots
+                } else {
+                    self?.todayTimeSlots = []
+                }
+                if let secondDay = response.scheduleDays.dropFirst().first {
+                    self?.tomorrowTimeSlots = secondDay.timeSlots
+                } else {
+                    self?.tomorrowTimeSlots = []
+                }
+
+                if self?.todayTimeSlots.isEmpty ?? true && self?.tomorrowTimeSlots.isEmpty ?? true {
+                    self?.errorMessage = "No appointments available."
+                    self?.title = "Sorry"
+                    self?.showErrorDialog = true
+                }
+            }
+            .store(in: &cancellables)
+    }
+
+   
+
+    private func handleError(_ error: Error) {
+        title = "Error"
+        errorMessage = (error.localizedDescription)
+        showErrorDialog = true
+    }
+   
+}
